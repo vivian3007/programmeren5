f8a271e177b543b9dccfacbe8d4a81d0ca059438
vivian <1022620@hr.nl>
2022-09-29 21:23:29 +0200

    create drawings & drawing view

---
+++ resources/views/drawings.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts/web')
+@section('content')
+    <a href="/">Drawings!!!</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'show']);
 
-Route::get('/home', [HomeController::class, 'show']);
+//Route::get('/home', [HomeController::class, 'show']);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/drawings', [App\Http\Controllers\DrawingsController::class, 'show'])->name('drawings');
+
+Route::get('/drawing', [App\Http\Controllers\DrawingController::class, 'show'])->name('drawing');
