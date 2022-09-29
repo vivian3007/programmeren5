91821ee0d2626cac34f4269b3ad48cd9268e0466
vivian <1022620@hr.nl>
2022-09-29 15:41:58 +0200

    create table drawings

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [WelcomeController::class, 'show']);
 
 Route::get('/home', [HomeController::class, 'show']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
