3ec546e43663b83f4e61e07ed5c0794666338cfd
vivian <1022620@hr.nl>
2022-10-05 22:32:38 +0200

    show table in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,23 @@
 @extends('layouts.app')
 @section('content')
     <a href="/drawings">index page</a>
+
+    <table class="table">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Materials</th>
+            <th>Details</th>
+            <th>Image</th>
+        </tr>
+        <tr>
+            @foreach($drawings as $drawing)
+                <td>{{ $drawing->id }}</td>
+                <td>{{ $drawing->name }}</td>
+                <td>{{ $drawing->materials }}</td>
+                <td>{{ $drawing->details }}</td>
+                <td>{{ $drawing->image }}</td>
+            @endforeach
+        </tr>
+    </table>
 @endsection
