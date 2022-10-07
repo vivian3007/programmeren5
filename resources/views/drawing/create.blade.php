5037fb1ea16c08837f5b9b75e720d2a4a65d344b
vivian <1022620@hr.nl>
2022-10-07 13:58:56 +0200

    create new drawing + in database

--- resources/views/drawing/create.blade.php
+++ resources/views/drawing/create.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.app')
 @section('content')
-    <a href="/drawings">Drawing</a>
+    <form action="{{ route('drawing.store') }}" method="POST">
+        @csrf
+        <label for="name">Title:</label>
+        <input type="text" id="name" name="name"><br><br>
+        <label for="materials">Materials:</label>
+        <input type="text" id="materials" name="materials"><br><br>
+        <label for="details">Details:</label>
+        <input type="text" id="details" name="details"><br><br>
+        <input type="submit" value="Submit">
+    </form>
 @endsection
